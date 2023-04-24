e610f9b5a4743d43e6a7b037bccb6a16ac8dd760
HerBaXs <ern.herbaxs@gmail.com>
2023-04-24 14:04:52 +0400

    barbershop

---
+++ application/controllers/User_Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+class User_Controller extends CI_Controller {
+
+    public function index(){
+        echo "Bu Index Metodudur.";
+    }
+
+    public function about(){
+        echo "Bu About metodudur.";
+    }
+
+    public function contact(){
+        echo "Bu Contact metodudur.";
+    }
+
+}
